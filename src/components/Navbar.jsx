6e5570d123f9f65988070fa152a4a016034fd65c
subhammahanty235 <subhammahanty235@gmail.com>
2022-06-26 20:00:55 +0530

    bug in navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,7 @@ function Navbar() {
             <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#e3f2fd"}}>
                 <div className="container-fluid">
                     <a className="navbar-brand navbar-header" href="/"> QuizziPlay</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    
                     <a href='https://github.com/subhammahanty235/Quizzi-Quiz-App' className="btn btn-outline-success btn-md">Github</a>
                 </div>
             </nav>
